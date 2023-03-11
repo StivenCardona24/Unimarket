cfd04387d4b7d0d738d75bed44c278a0b61be7ba
Stiven Cardona <stiven.cardona.0924@gmail.com>
2023-03-11 18:48:17 -0500

    nuevo

---
+++ src/main/java/co/edu/uniquindio/proyecto/UnimarketAplicacion.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.proyecto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class UnimarketAplicacion {
+    public static void main(String[] args) {
+        SpringApplication.run(UnimarketAplicacion.class, args);
+    }
+}
