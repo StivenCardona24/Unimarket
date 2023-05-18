5ef4b75fbfdfb80cca53382cea46409cb34cdca9
Stiven Cardona <stiven.cardona.0924@gmail.com>
2023-05-18 00:39:13 -0500

    sin JWT

--- src/main/java/co/edu/uniquindio/proyecto/seguridad/WebSecurityConfig.java
+++ src/main/java/co/edu/uniquindio/proyecto/seguridad/WebSecurityConfig.java
@@ -23,7 +23,10 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.cors();
-        http.authorizeHttpRequests()
+
+        http.authorizeHttpRequests().anyRequest().permitAll();
+
+     /*   http.authorizeHttpRequests()
         .requestMatchers("/api/auth/**").permitAll();
         http.authorizeHttpRequests()
                 .requestMatchers(HttpMethod.DELETE,"/usuario/**").hasRole("MODERADOR");
@@ -31,11 +34,11 @@ public class WebSecurityConfig {
           //      .requestMatchers("/productos/favorito/**").hasRole("CLIENTE");
         http.authorizeHttpRequests()
         .requestMatchers(HttpMethod.GET, "/api/productos/**").permitAll().anyRequest().authenticated();
-
-        http.exceptionHandling().authenticationEntryPoint(jwtEntryPoint);
+*/
+        /*http.exceptionHandling().authenticationEntryPoint(jwtEntryPoint);
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authenticationProvider(authenticationProvider);
-        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);*/
         return http.build();
     }
 }
